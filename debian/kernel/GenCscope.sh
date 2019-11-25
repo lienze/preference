5fc02431b334121169ba96da6db59f33555b1ba9
Enze Li <lienze2010@126.com>
2019-11-25 16:16:55 +0800

    added para to specify arch

--- debian/kernel/GenCscope.sh
+++ debian/kernel/GenCscope.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+arch="i386"
+if [ "$1" ];then
+	arch=$1
+fi
+
 if [ -f "./cscope.files" ];then
 	rm ./cscope*
 fi
 find ./ -path "./arch" -prune -o -path "./include" -prune -o -name "*.[chS]" > cscope.files
 sed -i "/./arch/d;/./include/d" cscope.files > cscope.files
-find ./arch/i386 -name "*.[chS]" >> cscope.files
-find ./include/asm-i386 -name "*.[chS]" >> cscope.files
+find ./arch/$arch -name "*.[chS]" >> cscope.files
+find ./include/asm-$arch -name "*.[chS]" >> cscope.files
 cscope -bqk
 echo "finished!"
